bdfca6027b0bbe2604aef7c346687a5f008d3c5b
Dabeix <112570287+Dabeix@users.noreply.github.com>
2025-06-03 09:50:59 +0200

    Update Next.js config to allow images.pexels.com

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.pexels.com',
+        hostname: 'images.pexels.com',
         pathname: '/photos/**',
       },
     ],
